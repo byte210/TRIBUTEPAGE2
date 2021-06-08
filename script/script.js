8a685e74dfed4c72742b4f633f5a5aacdeb7454f
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-06-08 13:31:29 +0200

    side menu works - working on styles

--- script/script.js
+++ script/script.js
@@ -9,7 +9,6 @@ const menuHide = () => {
 const menuShow = () => {
     const sideMenu = document.querySelector('.navigation-mobile-container');
 
-    console.log('should show smth now');
     sideMenu.style.position = "absolute";
     sideMenu.style.top = "0px";
     sideMenu.style.left = "0px";
